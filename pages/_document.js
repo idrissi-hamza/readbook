03eb6e7986837a3422113c5d16a6a8833168c223
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-25 00:51:45 +0200

    apply gradient for the body

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className='bg-gradient-to-r from-slate-200 via-gray-200 to-zinc-300'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
