b1906fdd18903f18c812e0ad88cce939fe13c962
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-25 14:26:03 +0200

    implement pagination

--- pages/books.js
+++ pages/books.js
@@ -1,7 +1,23 @@
 import { fetcher } from "../lib/fetcher";
 import BookList from "../components/BookList";
+import useSWR from "swr";
+import { useState } from "react";
+import { Pagination } from "@mui/material";
+// import { useRouter } from 'next/router'
 
 const Books = ({ books }) => {
+  const [pageIndex,setPageIndex]=useState(1)
+  const { data } = useSWR(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/books?pagination[page]=${pageIndex}&pagination[pageSize]=2`,
+    fetcher,
+    { fallbackdata: books } //initial data is books got w/ SSR
+  ); 
+  console.log(books);
+  // const router = useRouter(); 
+  function handlePaginationChange(e, value) {
+    setPageIndex(value);
+    // router.push(`pagination/?page=${value}`, undefined, { shallow: true });
+  }
   return (
     <>
       <h1 className="text-4xl md:text-5xl font-bold leading-tighter mb-4">
@@ -9,9 +25,17 @@ const Books = ({ books }) => {
           Books
         </span>
       </h1>
-      {/* <pre> { JSON.stringify(books.data[0].attributes.name)}</pre> */}
-      {/* <BooksList books={books}/> */}
-      <BookList books={books} />
+     
+
+      <BookList books={data} />
+      <Pagination
+        count={books.meta.pagination.pageCount}
+        variant='outlined'
+        color='primary'
+        className='pagination'
+        page={pageIndex}
+        onChange={handlePaginationChange}
+      />
     </>
   );
 };
@@ -19,7 +43,7 @@ const Books = ({ books }) => {
 export default Books;
 
 export async function getServerSideProps() {
-  const books = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/books`);
+  const books = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/books?pagination[page]=1&pagination[pageSize]=2`);
 
   console.log(books);
   return {
